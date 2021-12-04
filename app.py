713b498547837906eb1ad0737c4e1ddb82cc3617
samsong2 <samsong2@illinois.edu>
2021-12-04 06:51:39 +0000

    simple

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask, request, render_template
 
 # creates the flask
-template_dir = os.path.abspath('./src')
-app = Flask(__name__, template_folder)
+app = Flask(__name__)
 
 # The view function index() is linked to the main route using the app.route() decorator.
 # When the main route is requested, Flask will serve the request by calling index() and using its return value as the response.
