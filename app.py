b6177e6f6f9d696a01d94f2541830999793b6955
samsong2 <samsong2@illinois.edu>
2021-12-04 06:23:50 +0000

    test

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask
 
 # creates the flask
 app = Flask(__name__)
-def create_app():
-    app = Flask(__name__)
-
-    return app
 
 # The view function index() is linked to the main route using the app.route() decorator.
 # When the main route is requested, Flask will serve the request by calling index() and using its return value as the response.
 
 @app.route("/") # obviously the default page.
 def index():
-    return render_template('./src/website/index.html')
+    return render_template('./src/website/main.html')
+
+@app.route('/search', methods=['POST'])
+def my_form_post():
+    query = request.form['query'].lower()
+    if len(query) == 0:
+        return render_template('index.html')
+
+    print('querying', query)
+
+    #search_results = 
+    #total_results = 
+    return render_template('./src/website/results.html', search_results=search_results, num_results=len(), query=query, page_no=page_no, total_results=total_results)
